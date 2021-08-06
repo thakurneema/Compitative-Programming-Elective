c5a37868802bdaee62c4f04c3759307502b2f1c5
thakurneema <71770444+thakurneema@users.noreply.github.com>
2021-08-06 11:11:00 +0530

    done

--- topScorer.py
+++ topScorer.py
@@ -16,7 +16,27 @@
 
 def topScorer(data):
     # Your code goes here...
-    return ""
+    if(data==""):
+        return None
+    k=[]
+    l=[]
+    x=data.split('\n')
+    for i in range(len(x)-1):
+        k.append(x[i].split(","))
+    for j in range(len(k)):
+        l.append(k[j][0])
+        del k[j][0]
+    z=max(k[0])
+    y=max(k[1])
+    if z>y:
+        return l[0]
+    elif y>z:
+        return l[1]
+    else:
+        separator = ','
+        h=(str(separator.join(l)))
+        return h
+    
 
 data = '''\
 Fred,10,20,30,40
